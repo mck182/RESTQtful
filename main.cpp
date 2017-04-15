0adba8a100156861fc290af40e7aa8df87387ca7
Martin Klapetek <martin@500px.com>
2017-04-15 00:12:40 -0400

    Update the main.cpp example with the QObjectHelper usage

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <QCoreApplication>
 #include <QDebug>
 #include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonArray>
 
+#include "qobjecthelper.h"
+#include "example/pxphoto.h"
 #include "example/px500.h"
 
 int main(int argc, char *argv[])
@@ -10,14 +14,21 @@ int main(int argc, char *argv[])
 
     Px500 *pxServer = new Px500(&a);
 
+    qRegisterMetaType<PxPhoto*>("PxPhoto*");
+    qRegisterMetaType<PxUser*>("PxUser*");
+
     pxServer->photos([pxServer](const QJsonDocument &json, int statusCode) {
         qDebug() << "Photo GET return status:" << statusCode;
-        qDebug() << json;
         qDebug();
+        auto photosArray = json.object().value(QStringLiteral("photos"));
 
-        pxServer->photoComments(198209741, [](const QJsonDocument &json, int statusCode) {
-            qDebug() << "Comments for the photo:" << json;
-        });
+        Q_FOREACH (const QJsonValue &jsonPhoto, photosArray.toArray()) {
+            PxPhoto *photo = new PxPhoto();
+            QObjectHelper::qjsonobject2qobject(jsonPhoto.toObject(), photo);
+
+            qDebug() << photo->id() << photo->name() << photo->user()->fullname();
+        }
+
     });
 
     return a.exec();
