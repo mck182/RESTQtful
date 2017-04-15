57d5f7a411954d35e03f54ee1781d5cf773d970c
Martin Klapetek <martin@500px.com>
2017-04-15 00:39:02 -0400

    Add proper copyright headers to files

--- example/px500.h
+++ example/px500.h
@@ -1,3 +1,20 @@
+/*  This file is part of RESTQtful
+    Copyright (C) 2017  Martin Klapetek <mklapetek@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License version 2.1, as published by the Free Software Foundation.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 #ifndef PX500_H
 #define PX500_H
 
